8db04415343c7cb103bf4548f647f9daf09a9d29
SasoHdz <isaacrdzcmd@gmail.com>
2023-04-16 13:09:51 -0600

    First Commit

---
+++ src/main/java/com/serviciopaquetes/serviciopaquetes/ServicioPaquetesApplication.java
@@ -0,0 +1,13 @@
+package com.serviciopaquetes.serviciopaquetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioPaquetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioPaquetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serviciopaquetes/serviciopaquetes/ServicioPaquetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviciopaquetes.serviciopaquetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioPaquetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
